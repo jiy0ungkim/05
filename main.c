44a50a234fedca90396ef77c1700b2dbbc15355b
jiy0ungkim <2626kjy@naver.com>
2025-10-02 15:21:02 +0900

    02

--- main.c
+++ main.c
@@ -8,13 +8,11 @@ int main(int argc, char *argv[])
     printf("Input an integer : ");
     scanf("%d", &x);
     
-    if (x > 0)
-       printf("양수 입니다.\n");
-    else if (x < 0)
-       printf("음수입니다. \n");
+    if (x >= 0)
+       printf("절댓값은 %d입니다.\n", x);
     else
-       printf("0입니다.\n"); 
-    
+       printf("절댓값은 %d입니다. \n", -x);
+
     system("PAUSE");	
     return 0;
 }
