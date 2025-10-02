e261b2e5a5e1f157327aba48bbd4fcf17d840417
jiy0ungkim <2626kjy@naver.com>
2025-10-02 16:12:42 +0900

    06

--- main.c
+++ main.c
@@ -3,34 +3,23 @@
 
 int main(int argc, char *argv[])
 {
-    int x, y, result;
-    char op;
+    int x;
+    int answer = 59;
+    int trial = 0;
     
-    printf("Enter the calculation : ");
-    scanf("%d %c %d", &x, &op, &y);
-    
-    switch(op){
-               case '+':
-                    result = x+y;
-                    break;
-               
-               case '-':
-                    result = x-y;
-                    break;
-               
-               case '*':
-                    result = x*y;
-                    break;
-               
-               case '/':
-                    result = x/y;
-                    break;
-               
-               default:
-                    break;
-               }
-       
-    printf("= %d\n", result);
+    do{
+        printf("Guess a number: ");
+        scanf("%d", &x);
+        trial += 1;
+        
+        if (x > answer)
+           printf("high!\n");
+        else if (x < answer)
+           printf("low!\n"); 
+
+        }while(x != answer);
+        
+    printf("Congratulation! trials: %d\n", trial);
 
     system("PAUSE");	
     return 0;
